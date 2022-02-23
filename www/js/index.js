4055c30465983626fdc91768df7dfa66df07aa89
A80167811050629\alum-01 <adriacostasalse@gmail.com>
2022-02-23 19:17:33 +0100

    eliminar, localstorage

--- www/js/index.js
+++ www/js/index.js
@@ -20,10 +20,49 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
-
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
+    var x = localStorage.getItem("x");
+    var array = [];
+    if (x != "null") {
+        let datos = localStorage.getItem("Datos");
+        array = datos.split(',');
+        for (i in array) {
+            let newelem = $("<li><a href=''>"+array[i]+'</a><button class="deleteButton">Eliminar</button></li>');
+            $("ul").append(newelem);
+            $(".ui-listview").listview("refresh");
+        }
+    };
+
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    //document.getElementById('deviceready').classList.add('ready');
+    $( '#addButton' ).click(function() {
+        let text = $('#newtask').val();
+        array.push(text);
+        x=1;
+        //json.stringify(datos);
+        let newelem = $("<li><a href=''>"+text+'</a><button class="deleteButton">Eliminar</button></li>');
+        $("ul").append(newelem);
+        localStorage.setItem("Datos", array);
+        localStorage.setItem("x", x);
+        $(".ui-listview").listview("refresh");
+
+        $(".deleteButton").on('click', function() {
+            $(this).parent().remove();
+            const text = $(this).parent().text();
+            const text2 = text.slice(0, -8);
+            var myIndex = array.indexOf(text2);
+            if (myIndex !== -1) {
+                array.splice(myIndex, 1);
+            }
+            localStorage.setItem("Datos", array);
+            if (array.length == 0){
+                x = null;
+                localStorage.setItem("x", x);
+            }
+        });
+    });
+
+
 }
